da1125de9d6f655223b86a1e82773ef6038aaa1e
Revath S Kumar <rsk@revathskumar.com>
2022-12-20 11:45:36 +0100

    alias: add gbr to list the remote branches and switch

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -39,6 +39,7 @@ alias gca='git commit -a'
 alias gco='git checkout'
 #alias gb='git branch'
 alias gb='git branch | cut -c 3- | fzf --multi --preview="git log {} --"'
+alias gbr='git branch -rl "origin/*" | cut -c 10- | fzf | xargs git checkout'
 alias gbc='git branch | cut -c 3- | fzf --preview="git log {} --" | xargs git checkout'
 #compdef _git gdv=git-branch
 #compdef _git gco=git-branch
